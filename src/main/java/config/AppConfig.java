b1db2c1786195cb9709e1db972754da213d55e0b
rohitlavate97 <rohitlavate97@gmail.com>
2024-12-04 07:45:02 +0530

    Added 'basepackages' parameter, project started running successfully

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "beans")
 public class AppConfig {
 }
